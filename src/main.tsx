118993835deef7ace3150a791007b6e350c8e76d
Bluestar-coder <1255626166@qq.com>
2026-03-15 16:17:49 +0800

    perf: inline app shell bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode, Suspense, lazy } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/modules.css'
 import { i18nReady } from './i18n'
-import BootstrapFallback from './components/BootstrapFallback'
+import AppShell from './AppShell'
 import { installRuntimeWarningFilter } from './utils/runtime-warnings'
 
-const AppShell = lazy(() => import('./AppShell'))
 const sentryEnabled = import.meta.env.PROD && !!import.meta.env.VITE_SENTRY_DSN
 
 installRuntimeWarningFilter()
@@ -22,9 +21,7 @@ async function bootstrap() {
 
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      <Suspense fallback={<BootstrapFallback />}>
-        <AppShell />
-      </Suspense>
+      <AppShell />
     </StrictMode>,
   )
 }
